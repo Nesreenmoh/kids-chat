73a4f5feca3d3ab3e5049810a8c40a518aa9c1f4
NALSHARG <Layla01092025@>
2026-02-04 10:07:14 +0100

    ResponseMode added

---
+++ src/main/java/com/education/kids_chat/enums/ResponseMode.java
@@ -0,0 +1,10 @@
+package com.education.kids_chat.enums;
+/*
+DECISION CONTROL
+ */
+public enum ResponseMode {
+    NORMAL,
+    SUPPORTIVE,
+    REFUSAL,
+    CLARIFICATION
+}
---
+++ src/main/java/com/education/kids_chat/services/ContentSafetyService.java
@@ -0,0 +1,9 @@
+package com.education.kids_chat.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ContentSafetyService {
+
+
+}
